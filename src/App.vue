295b23df89d220e44c2ec4260b8213a3ba807b26
FlorianWoelki <FlorianWoelki@gmx.de>
2018-12-26 14:20:07 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <l-map
+      :zoom=13
+      :center="[47.413220, -1.219482]"
+    >
+      <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
+      <l-marker :lat-lng="[47.413220, -1.219482]"></l-marker>
+    </l-map>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+    //
+  },
+  data() {
+    return {
+      //
+    };
+  }
+};
+</script>
---
+++ src/main.js
@@ -0,0 +1,26 @@
+import Vue from 'vue';
+import './plugins/vuetify';
+import App from './App.vue';
+import router from './router';
+
+import { L, LMap, LTileLayer, LMarker } from 'vue2-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+Vue.component('l-map', LMap);
+Vue.component('l-tile-layer', LTileLayer);
+Vue.component('l-marker', LMarker);
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app');
