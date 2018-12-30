0af0e133ecfc731d4d4aae3f6e5a2de39179edb2
FlorianWoelki <FlorianWoelki@gmx.de>
2018-12-30 18:12:41 +0100

    Added marker icons and updated information page with content

--- src/views/Information.vue
+++ src/views/Information.vue
@@ -1,11 +1,99 @@
 <template>
-  <div class="information">
-    <h1>Information!</h1>
+  <div class="information container">
+    <v-data-table
+      :headers="headers"
+      :items="driver"
+      item-key="name"
+      class="elevation-1"
+    >
+      <template
+        slot="items"
+        slot-scope="props"
+      >
+        <tr @click="props.expanded = !props.expanded">
+          <td>
+            <v-avatar
+              :size="65"
+              color="grey lighten-4"
+            >
+              <img
+                :src="props.item.avatar"
+                alt="avatar"
+              >
+            </v-avatar>
+            {{ props.item.name }}
+          </td>
+          <td class="text-xs-right">{{ props.item.distance }}</td>
+          <td class="text-xs-right">{{ props.item.price }}€</td>
+          <td class="text-xs-right">{{ props.item.car }}</td>
+        </tr>
+      </template>
+      <template
+        slot="expand"
+        slot-scope="props"
+      >
+        <v-card flat>
+          <v-card-text>
+            <v-btn color="success">Call {{ props.item.name }} to You!</v-btn>
+            <v-btn color="error">Report driver</v-btn>
+          </v-card-text>
+        </v-card>
+      </template>
+    </v-data-table>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Information'
+  name: 'Information',
+  data() {
+    return {
+      headers: [
+        {
+          text: 'Name',
+          align: 'left',
+          sortable: false,
+          value: 'name'
+        },
+        { text: 'Distance', value: 'distance', align: 'right' },
+        { text: 'Price (€ / minute)', value: 'price', align: 'right' },
+        { text: 'Car', value: 'car', align: 'right' }
+      ],
+      driver: [
+        {
+          value: false,
+          name: 'Peter Gregon',
+          avatar: 'https://randomuser.me/api/portraits/men/44.jpg',
+          distance: 3.75,
+          price: 0.75,
+          car: 'Mercedes'
+        },
+        {
+          value: false,
+          name: 'Alex Grosh',
+          avatar: 'https://randomuser.me/api/portraits/men/18.jpg',
+          distance: 1.09,
+          price: 1.75,
+          car: 'Bentley'
+        },
+        {
+          value: false,
+          name: 'Ryan McLey',
+          avatar: 'https://randomuser.me/api/portraits/men/28.jpg',
+          distance: 2,
+          price: 0.9,
+          car: 'Mercedes'
+        }
+      ]
+    };
+  }
 };
 </script>
+
+<style lang="scss">
+.information {
+  .v-avatar {
+    margin: 10px;
+  }
+}
+</style>
